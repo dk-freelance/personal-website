f9a5bdabbf73bae7aae7c1d9f46576f250964b1b
dk-freelance <dknichel.freelance@gmail.com>
2024-11-27 12:29:47 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.untangling.tech",
+  site: "https://untangling-tech.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
